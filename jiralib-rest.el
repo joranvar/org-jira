ebfe05e050311a712daddfc47aeeaf0ad2a0d624
joranvar <bart.post@gmail.com>
2015-02-14 22:45:06 +0100

    Fix comment and example of rest-json-sync-call
    The examples showed DATA as a separate plist, which can now be an inline
    plist. Also, the result was still incorrectly shown as a plist, which
    has been changed to an alist in commit 551b03e.

--- jiralib-rest.el
+++ jiralib-rest.el
@@ -10,19 +10,19 @@
 (defun rest-json-sync-call (base-url resource &optional method &rest data)
   "Call the REST method on BASE-URL/RESOURCE.
 
-The http method METHOD is used, defaulting to GET.
-If given, the alist DATA is encoded to json and sent along.
+The http method METHOD is used, defaulting to GET.  If present,
+the extra DATA (as a plist) is encoded to json and sent along.
 
 Returns a cons of the response status code and the response data,
-also encoded as a alist.
+encoded as a alist.
 
 Example:
 
   (rest-json-sync-call \"http://rest.example.com\" \"/thing\"
                        \"POST\"
-                       '(:name \"myThing\" :color \"blue\"))
+                       :name \"myThing\" :color \"blue\")
 
-  ⇒ (200 :thing (:id 1 :name \"myThing\" :color \"blue\"))"
+  ⇒ (200 . (thing (id . 1) (name . \"myThing\") (color . \"blue\")))"
   (let ((request (request
 		  (concat base-url resource)
 		  :type (or method "GET")
