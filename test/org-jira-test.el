dd6f56e28032aa8aa83585c3b82265e71cbcbd84
joranvar <bart.post@gmail.com>
2015-02-15 11:19:05 +0100

    Create macro "using-fake-call" and test it
    This macro will temporarily replace the function cell for a symbol with
    a call to the given body. This will come in handy when testing out
    several scenarios in REST calls and replies that I'd rather not test
    against a real server.

---
+++ test/org-jira-test.el
@@ -0,0 +1,12 @@
+
+
+;;; Code:
+(ert-deftest using-fake-environment-works-fine ()
+  "Exploratory test to make sure that real REST calls are not made."
+  (should (equal
+	   (using-fake-call 'jira-call
+	       ("New return")
+	     (jira-session-delete))
+	   "New return")))
+
+;;; org-jira-test.el ends here
---
+++ test/test-helper.el
@@ -0,0 +1,16 @@
+
+(require 'f)
+(require 'jiralib-rest (f-expand "jiralib-rest.el" (f-parent (f-dirname (f-this-file)))))
+
+;;; Code:
+(defmacro using-fake-call (fsymbol new-body &rest body)
+  "Redirects calls to FSYMBOL to the NEW-BODY throughout BODY."
+  (declare (indent 2))
+  `(let ((old-call))
+    (fset old-call ,fsymbol)
+    (fset ,fsymbol (lambda (&rest args) ,@new-body))
+    (let ((result ,@body))
+      (fset ,fsymbol old-call)
+      result)))
+
+;;; test-helper.el ends here
